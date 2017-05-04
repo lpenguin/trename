f29bc73c4ae999dadf18422c1cb4c6ed557ad3b7
Nikita <iamlilacpenguin@gmail.com>
2017-05-04 12:43:12 +0300

    added debug messages

--- trename/cli.py
+++ trename/cli.py
@@ -1,5 +1,5 @@
 import argparse
-from trename.trename import new_name
+from trename.trename import new_name, to_re_in, to_re_out
 from os import rename
 import sys
 
@@ -8,13 +8,22 @@ def main():
     p = argparse.ArgumentParser()
     p.add_argument('-v', '--verbose', action='store_true')
     p.add_argument('-n', '--dry-run', action='store_true')
+    p.add_argument('-D', '--debug', action='store_true')
     p.add_argument('from_tpl')
     p.add_argument('to_tpl')
     p.add_argument('files', nargs='*')
 
     args = p.parse_args()
+
+    if args.debug:
+        re_in = to_re_in(args.from_tpl)
+        re_out = to_re_out(args.to_tpl)
+        print('Regexp from: "{}"\nRegexp to:   "{}"'.format(re_in, re_out))
+
     for filename in args.files:
         to_filename = new_name(filename, args.from_tpl, args.to_tpl)
+        if args.debug:
+            print("Trying: "+filename)
         if to_filename is not None:
             if args.dry_run:
                 print("{filename} will be renamed to {to_filename}".format(
--- trename/trename.py
+++ trename/trename.py
@@ -10,12 +10,13 @@ def to_re_out(tpl):
 
 
 def new_name(filename, tpl_in, tpl_out):
-    re_in = to_re_in(tpl_in)
+    re_in = to_re_in(tpl_in.replace('.', '\.'))
     re_out = to_re_out(tpl_out)
 
     if not re.match(re_in, filename):
         return None
     try:
         return re.sub(re_in, re_out, filename)
-    except IndexError:
+    except IndexError as ex:
+        print(ex)
         return None
